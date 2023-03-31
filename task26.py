223aceaaf2e05b2a78f984756122079b72b1a4f8
Valentin.Kostin <frezer.2021@mail.ru>
2023-03-31 08:58:49 +0300

    task 26 rabotaet

--- task26.py
+++ task26.py
@@ -6,12 +6,13 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8
 
-def degree_num(a,b):    
+def degree_num(a, b):
     if b == 0:
         return 1
     return a*degree_num(a, b-1)
 
+
 numA = int(input('введите чисело А: '))
 numB = int(input('введите чисело B: '))
 
-print(degree_num(numA,numB))
+print(degree_num(numA, numB))
